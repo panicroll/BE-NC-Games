c1c39cf5c8c57955fefd07833c2996fecd1a48e7
sophie <sophieleishman@outlook.com>
2022-07-06 17:09:15 +0100

    5 PATCH review completed with error handling

--- controllers/controller.js
+++ controllers/controller.js
@@ -1,18 +1,36 @@
-const {fetchCategories, fetchReview} = require("../models/model")
+const {
+  fetchCategories,
+  fetchReview,
+  updateReview,
+} = require("../models/model");
 
 const getCategories = (req, res) => {
-    fetchCategories().then((categories) => {
-        res.status(200).send({categories})
-    })
-}
+  fetchCategories().then((categories) => {
+    res.status(200).send({ categories });
+  });
+};
 
 const getReview = (req, res, next) => {
-    const {review_id} = req.params;
-    fetchReview(review_id).then((review) => {
-        res.status(200).send({review})
-    }).catch((err) => {
-        next(err);
+  const { review_id } = req.params;
+  fetchReview(review_id)
+    .then((review) => {
+      res.status(200).send({ review });
     })
-}
+    .catch((err) => {
+      next(err);
+    });
+};
 
-module.exports = {getCategories, getReview}
+const patchReview = (req, res, next) => {
+  const { inc_votes } = req.body;
+  const { review_id } = req.params;
+  updateReview(inc_votes, review_id)
+    .then((review) => {
+      res.status(200).send({ review });
+    })
+    .catch((err) => {
+      next(err);
+    });
+};
+
+module.exports = { getCategories, getReview, patchReview };
--- models/model.js
+++ models/model.js
@@ -1,23 +1,36 @@
 const db = require("../db/connection");
 
 const fetchCategories = () => {
-    return db.query(
-        `SELECT * FROM categories;`
-    ).then(({rows}) => {
-        return rows;
-    })
-}
+  return db.query(`SELECT * FROM categories;`).then(({ rows }) => {
+    return rows;
+  });
+};
 
 const fetchReview = (review_id) => {
-    return db.query(
-        `SELECT * FROM reviews WHERE review_id = $1;`, [review_id]
-    ).then(({rows}) => {
-        const review = rows[0];
-        if(!review) {
-            return Promise.reject({status: 404, message: "Review not found"})
-        }
-        return review;
-    })
-}
+  return db
+    .query(`SELECT * FROM reviews WHERE review_id = $1;`, [review_id])
+    .then(({ rows }) => {
+      const review = rows[0];
+      if (!review) {
+        return Promise.reject({ status: 404, message: "Review not found" });
+      }
+      return review;
+    });
+};
 
-module.exports = {fetchCategories, fetchReview}
+const updateReview = (inc_votes, review_id) => {
+  return db
+    .query(
+      `UPDATE reviews SET votes = votes + $1 WHERE review_id = $2 RETURNING *;`,
+      [inc_votes, review_id]
+    )
+    .then(({ rows }) => {
+      const review = rows[0];
+      if (!review) {
+        return Promise.reject({ status: 404, message: "Review not found" });
+      }
+      return review;
+    });
+};
+
+module.exports = { fetchCategories, fetchReview, updateReview };
