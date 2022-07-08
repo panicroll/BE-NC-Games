ab0ad39661a7895c026889a4a768e7ed36911d94
sophie <sophieleishman@outlook.com>
2022-07-08 10:43:36 +0100

    7 GET review updated to retrieve comment_count

--- models/fetchReview.js
+++ models/fetchReview.js
@@ -2,7 +2,13 @@ const db = require("../db/connection");
 
 const fetchReview = (review_id) => {
   return db
-    .query(`SELECT * FROM reviews WHERE review_id = $1;`, [review_id])
+    .query(
+      `SELECT reviews.*, COUNT(comments.comment_id)::INT AS comment_count 
+      FROM reviews LEFT JOIN comments ON comments.review_id = reviews.review_id 
+      WHERE reviews.review_id = $1 GROUP BY reviews.review_id;
+      `,
+      [review_id]
+    )
     .then(({ rows }) => {
       const review = rows[0];
       if (!review) {
